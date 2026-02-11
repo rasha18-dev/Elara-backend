ef60439e6e2f9ef03c940bfd262303e4dc388881
Rasha Basim <rasha18.dev@gmail.com>
2026-02-10 21:46:00 -1200

    change hosting name in api

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const app = express();
 // ✅ CORS (Frontend Vite URL)
 app.use(
   cors({
-    origin:["http://localhost:5173", "http://localhost:5174","https://elara-delta.vercel.app"],
+    origin:["http://localhost:5173", "http://localhost:5174","https://rumea.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
